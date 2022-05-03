e3b9408d19e90578020dc3042f5787034fab0f58
Patrik Trefil <36563492+PatrikTrefil@users.noreply.github.com>
2022-05-03 11:37:03 +0200

    #13 programmatically created the schema (no returns yet)

--- src/apps/crawler-server/src/graphql.ts
+++ src/apps/crawler-server/src/graphql.ts
@@ -1,17 +1,62 @@
 import { graphqlHTTP } from "express-graphql";
-import { buildSchema } from "graphql";
+import {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLList,
+    GraphQLID,
+    GraphQLString,
+    GraphQLBoolean,
+    GraphQLNonNull,
+} from "graphql";
 
-const graphqlSchema = buildSchema(`
-type Query {
-    hello: String
-}
-`);
+const WebPageType = new GraphQLObjectType({
+    name: "WebPage",
+    fields: {
+        identifier: {
+            type: GraphQLNonNull(GraphQLID),
+        },
+        label: { type: GraphQLNonNull(GraphQLString) },
+        url: { type: GraphQLNonNull(GraphQLString) },
+        regexp: { type: GraphQLNonNull(GraphQLString) },
+        tags: {
+            type: GraphQLNonNull(GraphQLList(GraphQLNonNull(GraphQLString))),
+        },
+        active: { type: GraphQLNonNull(GraphQLBoolean) },
+    },
+});
 
-const root = { hello: () => "Hello world!" };
+const NodeType = new GraphQLObjectType({
+    name: "Node",
+    // fields are defined as a function, so we can reference NodeType
+    fields: () => ({
+        title: { type: GraphQLString },
+        url: { type: GraphQLNonNull(GraphQLString) },
+        crawlTime: { type: GraphQLString },
+        links: { type: GraphQLNonNull(GraphQLList(GraphQLNonNull(NodeType))) },
+        owner: { type: GraphQLNonNull(WebPageType) },
+    }),
+}) as GraphQLObjectType; // because of recursive types, we need to do an explicit cast
+
+const QueryType = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        websites: {
+            type: GraphQLList(WebPageType),
+            resolve: () => null,
+        },
+        nodes: {
+            type: GraphQLList(NodeType),
+            resolve: () => null,
+        },
+    },
+});
+
+const graphqlSchema = new GraphQLSchema({
+    query: QueryType,
+});
 
 const graphqlMiddleware = graphqlHTTP({
     schema: graphqlSchema,
-    rootValue: root,
     graphiql: true,
 });
 
