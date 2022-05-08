644d002258552de7d8183d8868f653a4d0eb6f85
Milan Lamplot <78499519+Flying-crypt@users.noreply.github.com>
2022-05-08 13:49:27 +0200

    run test

--- src/apps/crawler-server/src/index.ts
+++ src/apps/crawler-server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 const app = express();
 const port = 4000;
+import {runTest} from './execution-manager-test';
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello world!");
@@ -9,3 +10,6 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+
+runTest();
