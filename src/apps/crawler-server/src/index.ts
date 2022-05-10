f77949cf58685e67393f08756997a6598af865c1
Patrik Trefil <36563492+PatrikTrefil@users.noreply.github.com>
2022-05-10 14:13:00 +0200

    fix log message on start of crawler-server

--- src/apps/crawler-server/src/index.ts
+++ src/apps/crawler-server/src/index.ts
@@ -10,5 +10,5 @@ api.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`crawler-server listening on port ${port}`);
 });
