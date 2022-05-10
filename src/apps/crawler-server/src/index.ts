ef12a8386ab904693f356b92c0ba5ed6d880ccad
Patrik Trefil <patrik.trefil@gmail.com>
2022-05-10 21:28:44 +0200

    create api endpoints #7

--- src/apps/crawler-server/src/index.ts
+++ src/apps/crawler-server/src/index.ts
@@ -5,8 +5,28 @@ const port = 4000;
 const api = express.Router();
 app.use("/api", api);
 
-api.get("/", (req: Request, res: Response) => {
-    res.send("Hello world!");
+api.get("/records", (req: Request, res: Response) => {
+    res.send("records");
+});
+
+api.get("/records/:recordId(\\d+)", (req: Request, res: Response) => {
+    res.send("record: " + req.params.recordId);
+});
+
+api.post("/records/:recordId(\\d+)", (req: Request, res: Response) => {
+    res.send("record: " + req.params.recordId + " post");
+});
+
+api.put("/records/:recordId(\\d+)", (req: Request, res: Response) => {
+    res.send("record: " + req.params.recordId + " put");
+});
+
+api.delete("/records/:recordId(\\d+)", (req: Request, res: Response) => {
+    res.send("record: " + req.params.recordId + " delete");
+});
+
+api.get("/records/:recordId(\\d+)/start", (req: Request, res: Response) => {
+    res.send("record: " + req.params.recordId + " start");
 });
 
 app.listen(port, () => {
