649879016954f1c94707961ed8fa00359c0f5975
Patrik Trefil <patrik.trefil@gmail.com>
2022-05-11 11:11:11 +0200

    disable logging in production (performance boost) #7

--- src/apps/crawler-server/src/index.ts
+++ src/apps/crawler-server/src/index.ts
@@ -2,6 +2,15 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 4000;
 
+if (process.env.NODE_ENV === "production") {
+    // disable logging
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const emptyFunc = () => {};
+    console.log = emptyFunc;
+    console.info = emptyFunc;
+    console.debug = emptyFunc;
+}
+
 const api = express.Router();
 app.use("/api", api);
 
