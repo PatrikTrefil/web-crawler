d775145241c75b523c28863a5946bc2391dd8568
Patrik Trefil <patrik.trefil@gmail.com>
2022-05-15 10:55:28 +0200

    #5 made model class thread safe

--- src/apps/crawler-server/src/model.ts
+++ src/apps/crawler-server/src/model.ts
@@ -6,10 +6,8 @@ import {
     IWebPageLink,
 } from "ts-types";
 import IModel from "./IModel";
-import neo4j, { Driver } from "neo4j-driver";
+import neo4j, { Driver, Transaction } from "neo4j-driver";
 import "dotenv/config";
-
-// TODO: make this class thread-safe
 
 export default class Model implements IModel {
     private driver: Driver;
@@ -31,42 +29,51 @@ export default class Model implements IModel {
     // #region WebsiteRecords
     public async getRecordById(id: string): Promise<IWebsiteRecord | null> {
         const session = this.driver.session();
-        let record: IWebsiteRecord | null;
+        let websiteRecord: IWebsiteRecord | null;
         try {
-            const recordResult = await session.run(
-                "MATCH (record:Record { id: $id }) RETURN record",
-                { id: id }
-            );
-            const lastExecutionResult = await session.run(
-                "MATCH (record:Record { id: $id })-[lastExecution:Execution]-(:WebPage) RETURN lastExecution.id",
-                { id: id }
-            );
-            if (recordResult.records.length > 0) {
-                const lastExecutionId =
-                    lastExecutionResult.records.length > 0
-                        ? lastExecutionResult.records[0].get("lastExecution.id")
-                        : null;
-                const resObj = recordResult.records[0].get("record").properties;
-                record = {
-                    id: resObj.id,
-                    url: resObj.url,
-                    boundaryRegex: resObj.boundaryRegex,
-                    label: resObj.label,
-                    isActive: resObj.isActive,
-                    tags: resObj.tags,
-                    periodicityInSeconds: resObj.periodicityInSeconds,
-                    lastExecutionId: lastExecutionId,
-                };
-            } else {
-                record = null;
-            }
+            // we are using a transaction to always return a website record that has existed in the database at some point
+            // if we did not use a transaction, we could get a website record in our first query, then someone else deletes
+            // the website record and its lastExecution, then we query the lastExecution, we get null. We now return our websiteRecord
+            // without the lastExecution and this situation never existed in the database.
+            websiteRecord = await session.readTransaction(async (tx) => {
+                const recordResult = await tx.run(
+                    "MATCH (record:Record { id: $id }) RETURN record",
+                    { id: id }
+                );
+                let record: IWebsiteRecord | null = null;
+                if (recordResult.records.length > 0) {
+                    const lastExecutionResult = await tx.run(
+                        "MATCH (record:Record { id: $id })-[lastExecution:Execution]-(:WebPage) RETURN lastExecution.id",
+                        { id: id }
+                    );
+                    const lastExecutionId =
+                        lastExecutionResult.records.length > 0
+                            ? lastExecutionResult.records[0].get(
+                                  "lastExecution.id"
+                              )
+                            : null;
+                    const resObj =
+                        recordResult.records[0].get("record").properties;
+                    record = {
+                        id: resObj.id,
+                        url: resObj.url,
+                        boundaryRegex: resObj.boundaryRegex,
+                        label: resObj.label,
+                        isActive: resObj.isActive,
+                        tags: resObj.tags,
+                        periodicityInSeconds: resObj.periodicityInSeconds,
+                        lastExecutionId: lastExecutionId,
+                    };
+                }
+                return record;
+            });
         } catch (e) {
             session.close();
             throw e;
         } finally {
             session.close();
         }
-        return record;
+        return websiteRecord;
     }
     public async createRecord(
         url: string,
@@ -231,26 +238,18 @@ export default class Model implements IModel {
     }
     // #endregion
     // #region Crawlexecutions
-    // TODO: implement methods related to crawl executions
     /**
      * @returns true if the webpage was created; false if the webpage already exists
      */
-    private async createWebPage(url: string): Promise<boolean> {
-        const session = this.driver.session();
-        let numberOfCreatedNodes;
-        try {
-            const result = await session.run(
-                "MERGE (webpage:WebPage { url: $url })",
-                { url: url }
-            );
-            numberOfCreatedNodes =
-                result.summary.counters.updates()["nodesCreated"];
-        } catch (e) {
-            session.close();
-            throw e;
-        } finally {
-            session.close();
-        }
+    private async createWebPage(
+        url: string,
+        tx: Transaction
+    ): Promise<boolean> {
+        const result = await tx.run("MERGE (webpage:WebPage { url: $url })", {
+            url: url,
+        });
+        const numberOfCreatedNodes =
+            result.summary.counters.updates()["nodesCreated"];
         return numberOfCreatedNodes === 1;
     }
     public async createExecutionLink(
@@ -259,53 +258,40 @@ export default class Model implements IModel {
         title: string,
         crawlTime: Date
     ): Promise<string> {
-        // TODO: what happens if websiteRecordId is invalid
-        await this.createWebPage(webPageURL);
         const session = this.driver.session();
         let createdExecutionLinkId;
         try {
-            const result = await session.run(
-                `MATCH (a:Record { id: $websiteRecordId }), (b:WebPage { url: $webPageURL })
-                 MERGE
-                    (a)-[l:Link { id : apoc.create.uuid() }]->(b)
-                 MERGE
-                    (a)-[e:Execution]->(b)
-                 SET
-                    e.crawlTime = $crawlTime,
-                    e.id = apoc.create.uuid(),
-                    l.title = $title,
-                    l.crawlTime = $crawlTime,
-                    l.crawlExecutionId = e.id
-                 RETURN e.id`,
-                {
-                    websiteRecordId: websiteRecordId,
-                    webPageURL: webPageURL.toString(),
-                    title: title,
-                    crawlTime: neo4j.types.DateTime.fromStandardDate(crawlTime),
+            createdExecutionLinkId = await session.writeTransaction(
+                async (tx) => {
+                    await this.createWebPage(webPageURL, tx);
+                    const result = await tx.run(
+                        `MATCH (a:Record { id: $websiteRecordId }), (b:WebPage { url: $webPageURL })
+                            MERGE
+                                (a)-[l:Link { id : apoc.create.uuid() }]->(b)
+                            MERGE
+                                (a)-[e:Execution]->(b)
+                            SET
+                                e.crawlTime = $crawlTime,
+                                e.id = apoc.create.uuid(),
+                                l.title = $title,
+                                l.crawlTime = $crawlTime,
+                                l.crawlExecutionId = e.id
+                            RETURN e.id`,
+                        {
+                            websiteRecordId: websiteRecordId,
+                            webPageURL: webPageURL.toString(),
+                            title: title,
+                            crawlTime:
+                                neo4j.types.DateTime.fromStandardDate(
+                                    crawlTime
+                                ),
+                        }
+                    );
+                    if (result.records.length > 0)
+                        return result.records[0].get("e.id");
+                    else throw "websiteRecordId not found";
                 }
             );
-            if (result.records.length > 0) {
-                createdExecutionLinkId = result.records[0].get("e.id");
-            } else {
-                // find error
-                const recordResult = await session.run(
-                    "MATCH (record:Record { id: $websiteRecordId }) RETURN record",
-                    {
-                        websiteRecordId: websiteRecordId,
-                    }
-                );
-                if (recordResult.records.length === 0)
-                    throw "websiteRecordId not found";
-                const webPageResult = await session.run(
-                    "MATCH (webpage:WebPage { url: $webPageURL }) RETURN webpage",
-                    {
-                        webPageURL: webPageURL,
-                    }
-                );
-                if (webPageResult.records.length === 0)
-                    throw "webPageURL not found";
-                throw "Failed";
-            }
         } catch (e) {
             session.close();
             throw e;
@@ -321,31 +307,34 @@ export default class Model implements IModel {
         crawlTime: Date,
         crawlExecutionId: string
     ): Promise<string> {
-        await this.createWebPage(fromWebPageURL);
-        await this.createWebPage(toWebPageURL);
         const session = this.driver.session();
         let createdLinkId;
         try {
-            const result = await session.run(
-                `MATCH (fromWebPage:WebPage { url: $fromWebPageURL }), (toWebPage:WebPage { url: $toWebPageURL })
-                 MERGE (fromWebPage)-[e:Link {
-                    title : $title,
-                    crawlTime : $crawlTime,
-                    crawlExecutionId : $crawlExecutionId,
-                    id : apoc.create.uuid()
-                 }]->(toWebPage)
-                 RETURN e.id`,
-                {
-                    fromWebPageURL: fromWebPageURL,
-                    toWebPageURL: toWebPageURL,
-                    title: title,
-                    crawlTime: neo4j.types.DateTime.fromStandardDate(crawlTime),
-                    crawlExecutionId: crawlExecutionId,
-                }
-            );
-            if (result.records.length > 0)
-                createdLinkId = result.records[0].get("e.id");
-            else throw "Failed";
+            createdLinkId = await session.writeTransaction(async (tx) => {
+                await this.createWebPage(fromWebPageURL, tx);
+                await this.createWebPage(toWebPageURL, tx);
+                const result = await tx.run(
+                    `MATCH (fromWebPage:WebPage { url: $fromWebPageURL }), (toWebPage:WebPage { url: $toWebPageURL })
+                    MERGE (fromWebPage)-[e:Link {
+                        title : $title,
+                        crawlTime : $crawlTime,
+                        crawlExecutionId : $crawlExecutionId,
+                        id : apoc.create.uuid()
+                    }]->(toWebPage)
+                    RETURN e.id`,
+                    {
+                        fromWebPageURL: fromWebPageURL,
+                        toWebPageURL: toWebPageURL,
+                        title: title,
+                        crawlTime:
+                            neo4j.types.DateTime.fromStandardDate(crawlTime),
+                        crawlExecutionId: crawlExecutionId,
+                    }
+                );
+                if (result.records.length > 0)
+                    return result.records[0].get("e.id");
+                else throw "Failed";
+            });
         } catch (e) {
             session.close();
             throw e;
@@ -358,9 +347,10 @@ export default class Model implements IModel {
         const session = this.driver.session();
         let success;
         try {
-            // delete webpages nodes (and their relationships) that were present only in execution with given executionId
-            const lonelyWebpageDeleteResult = await session.run(
-                `MATCH (webPage:WebPage) WHERE
+            success = await session.writeTransaction(async (tx) => {
+                // delete webpages nodes (and their relationships) that were present only in execution with given executionId
+                const lonelyWebpageDeleteResult = await tx.run(
+                    `MATCH (webPage:WebPage) WHERE
                     exists(
                         (webPage)-[:Link { crawlExecutionId: $crawlExecutionId }]-()
                     ) AND
@@ -368,29 +358,31 @@ export default class Model implements IModel {
                         MATCH (webPage)-[r]-() WHERE NOT (r:Link AND r.crawlExecutionId = $crawlExecutionId)
                     }
                  DETACH DELETE webPage`,
-                { crawlExecutionId: crawlExecutionId }
-            );
-            const deleteAllLinksResult = await session.run(
-                `MATCH (recordOrWebPage)-[link:Link { crawlExecutionId: $crawlExecutionId }]->(webpage:WebPage)
+                    { crawlExecutionId: crawlExecutionId }
+                );
+                const deleteAllLinksResult = await tx.run(
+                    `MATCH (recordOrWebPage)-[link:Link { crawlExecutionId: $crawlExecutionId }]->(webpage:WebPage)
                  WHERE recordOrWebPage:Record OR recordOrWebPage:WebPage
                  DELETE link`,
-                { crawlExecutionId: crawlExecutionId }
-            );
-            const deleteExecutionRelationshipResult = await session.run(
-                `MATCH (record:Record)-[rel:Execution { id: $crawlExecutionId }]->(webpage:WebPage)
+                    { crawlExecutionId: crawlExecutionId }
+                );
+                const deleteExecutionRelationshipResult = await tx.run(
+                    `MATCH (record:Record)-[rel:Execution { id: $crawlExecutionId }]->(webpage:WebPage)
                  DELETE rel`,
-                { crawlExecutionId: crawlExecutionId }
-            );
-            success =
-                lonelyWebpageDeleteResult.summary.counters.updates()[
-                    "relationshipsDeleted"
-                ] > 0 ||
-                deleteAllLinksResult.summary.counters.updates()[
-                    "relationshipsDeleted"
-                ] > 0 ||
-                deleteExecutionRelationshipResult.summary.counters.updates()[
-                    "relationshipsDeleted"
-                ] > 0;
+                    { crawlExecutionId: crawlExecutionId }
+                );
+                return (
+                    lonelyWebpageDeleteResult.summary.counters.updates()[
+                        "relationshipsDeleted"
+                    ] > 0 ||
+                    deleteAllLinksResult.summary.counters.updates()[
+                        "relationshipsDeleted"
+                    ] > 0 ||
+                    deleteExecutionRelationshipResult.summary.counters.updates()[
+                        "relationshipsDeleted"
+                    ] > 0
+                );
+            });
         } catch (e) {
             session.close();
             throw e;
