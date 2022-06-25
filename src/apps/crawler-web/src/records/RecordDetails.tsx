b72c25ce31efb7aff982a7c443f9f15410e995af
Patrik Trefil <patrik.trefil@gmail.com>
2022-06-25 11:55:41 +0200

    #9 fix of displaying of isActive in record details

--- src/apps/crawler-web/src/records/RecordDetails.tsx
+++ src/apps/crawler-web/src/records/RecordDetails.tsx
@@ -13,7 +13,7 @@ export default function RecordDetails({
                 <li>URL: {recordToDisplay.url}</li>
                 <li>Label: {recordToDisplay.label}</li>
                 <li>Boundary regex: {recordToDisplay.boundaryRegex}</li>
-                <li>Is active?: {recordToDisplay.isActive}</li>
+                <li>Is active?: {recordToDisplay.isActive ? "yes" : "no"}</li>
                 <li>Periodicity: {recordToDisplay.periodicityInSeconds} s</li>
                 <li>Tags: {recordToDisplay.tags.join(", ")}</li>
             </List>
